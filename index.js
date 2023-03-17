b7a14668e8abc2ae682d4e5ac332472f67ee1701
jessywaweru <jessy.waweru@student.moringaschool.com>
2023-03-17 10:53:47 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(activity='roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+}
+function mondayWork(activity='go to the office'){
+return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(activity='*'){
+    return function(flair="special"){
+        return `You are ${activity}${flair}${activity}!`
+        
+    }
+    }
+  //  const encouragingPromptFunction = wrapAdjective("*")
+   // wrapAdjective("||")("a dedicated programmer")
